441ddaa4013b98ec04df717e5bac6c9a073dd8b0
John Wineman <jwineman@thecoalition.com>
2020-02-15 15:27:20 -0700

    Making the create UI number go upgit statusgit statusgit status

--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select, TextInput, Button } from "grommet";
 
 export const Create = () => {
-  return "Create";
+  const [type, setType] = React.useState("basic");
+  const [period, setPeriod] = React.useState(0);
+  const [amount, setAmount] = React.useState(0);
+  return (
+    <>
+      <div>Type:</div>
+      <Select
+        options={["basic", "pinata"]}
+        value={type}
+        onChange={({ option }) => setType(option)}
+      />
+      <div>Interval</div>
+      <TextInput
+        value={period}
+        onChange={event => setPeriod(event.target.value)}
+      />
+      {type === "pinata" ? (
+        <>
+          <div>Amount</div>
+          <TextInput
+            value={amount}
+            onChange={event => setPeriod(event.target.value)}
+          />
+        </>
+      ) : null}
+      <Button
+        label="Create Living Proof"
+        onClick={() => console.log("NUMBER GO UP")}
+      />
+    </>
+  );
 };
 
 export default Create;
