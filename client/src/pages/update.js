6f0a064fd42a7f01537f93a7de972edf88e95629
John Wineman <jwineman@thecoalition.com>
2020-02-15 16:34:52 -0700

    Update proof number go up

--- client/src/pages/update.js
+++ client/src/pages/update.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select, TextInput, Button } from "grommet";
 
 export const Update = () => {
-  return "Update";
+  const type = null;
+  const [period, setPeriod] = React.useState(0);
+  const [amount, setAmount] = React.useState(0);
+  return (
+    <>
+      <div>Interval</div>
+      <TextInput
+        value={period}
+        onChange={event => setPeriod(event.target.value)}
+      />
+      {type === "pinata" ? (
+        <>
+          <div>Amount</div>
+          <TextInput
+            value={amount}
+            onChange={event => setPeriod(event.target.value)}
+          />
+        </>
+      ) : null}
+      <Button
+        label="Update Living Proof"
+        onClick={() => console.log("NUMBER GO UP")}
+      />
+    </>
+  );
 };
 
 export default Update;
