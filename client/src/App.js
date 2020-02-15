9af0cef2c7f9e6db211a840509c3dc7971d8f356
John Wineman <jwineman@thecoalition.com>
2020-02-15 09:35:47 -0700

    Add Create, Update, Delete Proof Buttons.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Web3 from "./services/web3";
+import { Grommet, Anchor, Box, Button, Text } from "grommet";
 
 import "./App.css";
 
 class App extends Component {
-  state = { accounts: [], web3: null, connectError: null };
+  state = { accounts: [], web3: null, connectError: null, page: null };
 
   componentDidMount = async () => {
     try {
@@ -86,32 +87,71 @@ class App extends Component {
 
     if (this.state.connectError) {
       return (
-        <div className="App">
-          Metamask did not connect - {this.state.connectError.message}
-        </div>
+        <Grommet>
+          <Box
+            direction="row-responsive"
+            justify="center"
+            align="center"
+            pad="xlarge"
+            gap="medium"
+          >
+            Metamask did not connect - {this.state.connectError.message}
+          </Box>
+        </Grommet>
       );
     }
 
     if (!this.state.accounts || this.state.accounts.length === 0) {
       return (
-        <div className="App">
-          <button onClick={this.openMetaMask}>Connect via Metamask</button>
-        </div>
+        <Grommet plain>
+          <Box
+            direction="row-responsive"
+            justify="center"
+            align="center"
+            pad="xlarge"
+            gap="medium"
+          >
+            <Button label="Connect via Metamask" onClick={this.openMetaMask} />
+          </Box>
+        </Grommet>
       );
     }
 
     return (
-      <div className="App">
-        {this.state.values.map(values => {
-          return (
-            <div key={values.acct}>
-              {values.acct} (
-              {this.state.web3.utils.fromWei(values.amount, "ether")})
-            </div>
-          );
-        })}
-        <button onClick={this.disconnectAccounts}>Disconnect Account</button>
-      </div>
+      <Grommet plain>
+        <Box
+          direction="row-responsive"
+          justify="center"
+          align="center"
+          pad="xlarge"
+          gap="medium"
+        >
+          <Button
+            label="Button"
+            label="Create Proof"
+            onClick={() => this.setState({ page: "create" })}
+          />
+          <Button
+            label="Button"
+            label="Update Proof"
+            onClick={() => this.setState({ page: "update" })}
+          />
+          <Button
+            label="Button"
+            label="Delete Proof"
+            onClick={() => this.setState({ page: "delete" })}
+          />
+        </Box>
+        <Box
+          direction="row-responsive"
+          justify="center"
+          align="center"
+          pad="xlarge"
+          gap="medium"
+        >
+          <div>{this.state.page === null ? "Content" : this.state.page}</div>
+        </Box>
+      </Grommet>
     );
   }
 }
